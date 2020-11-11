b4c1fccf8de7e62703ba0049663607c2dff00b91
leminhtri <leminhtri900112@gmail.com>
2020-11-11 19:31:53 +0700

    fisnish pagination

--- controller/product.control.js
+++ controller/product.control.js
@@ -1,7 +1,11 @@
 var db = require('../lowdb')
 
 module.exports.product = function(req, res) {
+    var page = parseInt(req.query.page) || 1
+    var perPage = 8;
+    var start = (page - 1) * perPage;
+    var end = page * perPage;
     res.render('users/product',{
-        products: db.get('products').value()
+        products: db.get('products').value().slice(start,end)
     })
 }
